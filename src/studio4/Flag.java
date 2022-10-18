69597646572b589434aae4c78a8c1d774eca5a16
yuxin <yuxin@YuxinMouHP.dhcp.wustl.edu>
2022-10-18 11:52:25 -0500

    abab

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	
+		Color color = new Color(255, 0, 0);
+		StdDraw.setPenColor(color);
+	
+				StdDraw.filledRectangle(0.5, 0.5, 0.6, 0.3);
+				
+				Color x = new Color(0, 0, 0);
+				StdDraw.setPenColor(x);
+				StdDraw.text(0.5, 0.5,"習");
 	}
 }
